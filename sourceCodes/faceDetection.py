86271a008466ea92647e416975f2ffa0460c1945
zakaria <erfan.yekezare@gmail.com>
2022-05-13 22:33:51 +0430

    it figures out faceses. prints out the prediction score

--- sourceCodes/faceDetection.py
+++ sourceCodes/faceDetection.py
@@ -22,12 +22,20 @@ while True:
     if results.detections:
         for id, detection in enumerate(results.detections):
             bboxC = detection.location_data.relative_bounding_box
-            bbox = int(bboxC.xmin), int(bboxC.ymin), int(bboxC.width), int(bboxC.height)
-            print(bbox)
-            #cv2.rectangle(frameRgb, pt1, pt2,
-            #              (255, 0, 255), 2, cv2.FONT_HERSHEY_COMPLEX)
+            y, x, c = frameRgb.shape
+            bbox = int(bboxC.xmin*x), int(bboxC.ymin*y), int(bboxC.width*x), int(bboxC.height*y)
+            
+            #----Tarsim moraba baray chehre ha.
+            cv2.rectangle(frame, bbox, (255, 0, 255),
+                          2, cv2.FONT_HERSHEY_COMPLEX)
+            
+            #----darj darsad movafaghiat dar tashkhis chehre ha.
+            cv2.putText(frame, str(f' { int(detection.score[0]*100) }%'),
+                        (bbox[0]-20, bbox[1]),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 1)
+            
     
-    cv2.imshow("Frame", frameRgb)
+    cv2.imshow("Frame", frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
