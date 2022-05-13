a7aba8de44033e010a0c1f1a7b3e91e797ed77d4
zakaria <erfan.yekezare@gmail.com>
2022-05-13 16:14:06 +0430

    edir captured video specs to 1080p

--- faceDirection.py
+++ faceDirection.py
@@ -5,15 +5,14 @@ import numpy as np
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml') 
 cap = cv2.VideoCapture(0)
-cap.set(3, 720)
-cap.set(4, 1080)
+cap.set(3, 1080)
+cap.set(4, 1920)
 cap.set(10, 100)
 
 while True:
     success, img = cap.read()
     grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(grayImg, 1.3, 5)
-    print(faces)
     for (x, y, w, h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2) 
         roi_gray = grayImg[y:y+h, x:x+w]
