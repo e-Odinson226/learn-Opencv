34aed6031fce7c4237ce4fe47509b78e02e2d7a6
zakaria <erfan.yekezare@gmail.com>
2022-05-14 14:37:42 +0430

    working fine

---
+++ sourceCodes/faceMesh.py
@@ -0,0 +1,46 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1080)
+cap.set(4, 720)
+
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh()
+
+mpDrawingUtils = mp.solutions.drawing_utils
+mpDrawingSpec = mpDrawingUtils.DrawingSpec( (0, 128, 0), thickness = 1, circle_radius = 1)
+
+mpDrawingStyles = mp.solutions.drawing_styles
+mpDrawingStyles = mpDrawingStyles.get_default_face_mesh_tesselation_style()
+
+pTime = 0
+while True:
+    success, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    frameRgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = faceMesh.process(frameRgb)
+    
+    if result.multi_face_landmarks:
+        for faceLandmark in result.multi_face_landmarks:
+            
+            mpDrawingUtils.draw_landmarks(
+                frame,
+                faceLandmark,
+                connections = mpFaceMesh.FACEMESH_CONTOURS,
+                landmark_drawing_spec = mpDrawingSpec,
+                connection_drawing_spec = mpDrawingSpec
+                )
+    
+    #else:
+    #    pass
+    
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    
+    cv2.putText(frame, str(f'fps:{int(fps)}'),(5, 30), cv2.FONT_HERSHEY_PLAIN, 2, (50, 100, 50), 2)
+    cv2.imshow("Legion Eye", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
